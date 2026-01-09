b1ab1b5df6577df50411bfe6cd36cf7a8a7f09c1
aashu874 <mdaashif001112@gmail.com>
2026-01-09 22:37:28 +0530

    finally correct

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -10,7 +10,7 @@ app.use(cors({
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
 
-require("./config/mongoose-connect.js");
+require("./config/mongoose-connect");
 
 app.use(cookieParser());
 app.use(express.json());
